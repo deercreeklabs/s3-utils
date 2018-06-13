c73b41151cd7c8da1b18615bbcc061570dde1d73
Chad Harrington <chad@deercreeklabs.com>
2018-06-13 13:24:49 -0600

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deercreeklabs/s3-utils "0.1.2-SNAPSHOT"
+(defproject deercreeklabs/s3-utils "0.1.2"
   :description "Utilities for working with S3"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
