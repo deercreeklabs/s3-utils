c1c04ee7fd6f3c3dd8623ce77ebd1fb03d28fa96
Chad Harrington <chad@deercreeklabs.com>
2018-06-15 17:16:54 -0600

    Upgrade deps

--- project.clj
+++ project.clj
@@ -28,13 +28,13 @@
    :uberjar {:aot :all}}
 
   :dependencies
-  [[com.amazonaws/aws-java-sdk-s3 "1.11.346"
+  [[com.amazonaws/aws-java-sdk-s3 "1.11.348"
     :exclusions [com.fasterxml.jackson.core/jackson-core joda-time]]
    [com.taoensso/timbre "4.10.0" :exclusions [org.clojure/tools.reader]]
    [commons-logging/commons-logging "1.2"]
-   [deercreeklabs/async-utils "0.1.9"]
-   [deercreeklabs/baracus "0.1.6"]
-   [deercreeklabs/log-utils "0.1.6"]
+   [deercreeklabs/async-utils "0.1.10"]
+   [deercreeklabs/baracus "0.1.8"]
+   [deercreeklabs/log-utils "0.1.7"]
    [org.clojure/core.async "0.4.474"]
    [org.clojure/clojure "1.9.0"]
    [prismatic/schema "1.1.9"]]
