a62ee0bb0cf06089f9aec47c0d2461e422a78bd1
Chad Harrington <chad@deercreeklabs.com>
2018-10-16 21:42:36 -0600

    lein-release plugin: bumped version from 0.3.0 to 0.3.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -44,7 +44,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/s3-utils "0.3.0"
+(defproject deercreeklabs/s3-utils "0.3.1-SNAPSHOT"
   :description "Utilities for working with S3"
   :url "https://github.com/deercreeklabs/s3-utils"
   :license {:name "Apache License, Version 2.0"
